408e9a19b3f278f5e46414b2d55c4f4183ad81c9
NguyenPhust9 <1150080030@sv.hcmunre.edu.vn>
2026-09-21 11:36:54 +0700

    Fix production static files on Vercel

--- config/settings.py
+++ config/settings.py
@@ -121,7 +121,7 @@ STORAGES = {
         "BACKEND": (
             "django.contrib.staticfiles.storage.StaticFilesStorage"
             if DEBUG
-            else "whitenoise.storage.CompressedManifestStaticFilesStorage"
+            else "whitenoise.storage.CompressedStaticFilesStorage"
         )
     },
 }
